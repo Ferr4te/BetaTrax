e3a736753065026f34892e693be6d2a1fe5e4472
ouo <laicy0q0@connect.hku.hk>
2026-03-28 03:37:32 +0800

    update ForeignKey

--- BetaTrax/models.py
+++ BetaTrax/models.py
@@ -47,7 +47,22 @@ class DefectReport(models.Model):
     severity = models.CharField(max_length=10, choices=Severity.choices, null=True, blank=True)
     priority = models.CharField(max_length=10, choices=Priority.choices, null=True, blank=True)
 
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    betatester = models.ForeignKey(BetaTester, on_delete=models.CASCADE)
-    productowner = models.ForeignKey(ProductOwner, on_delete=models.CASCADE)
-    developer = models.ForeignKey(Developer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, 
+                                on_delete=models.CASCADE,
+                                related_name='defect_reports'
+                               )
+    
+    betatester = models.ForeignKey(BetaTester,
+                                   on_delete=models.CASCADE,
+                                  related_name='defect_reports'
+                                  )
+    
+    productowner = models.ForeignKey(ProductOwner, 
+                                     on_delete=models.CASCADE,
+                                     related_name='defect_reports'
+                                    )
+    
+    developer = models.ForeignKey(Developer, 
+                                  on_delete=models.CASCADE,
+                                 related_name='defect_reports'
+                                 )
