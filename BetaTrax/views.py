c78d3b0394fd41dd675351e7491b19b80f2239a0
ouo <laicy0q0@connect.hku.hk>
2026-03-28 23:41:47 +0800

    Update views.py

--- BetaTrax/views.py
+++ BetaTrax/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse, redirect
+from .forms import DefectForm
 # Create your views here.
+
+# this is dashboard view function for tester
+def dashboard_view(request):
+    return render(request, "tester/dashboard.html" )
+
+# handle the defect report form
+def defect_view(request):
+    if request.method == "POST":
+        form = DefectForm(request.POST)
+        if form.is_valid():
+            return redirect('defect-success')
+    else:
+        form = DefectForm()
+    context = {'form': form}
+    return render(request, 'tester/defectform.html', context)
+
+# handle the successful form submission page
+def defect_success_view(request):
+    return render(request, 'tester/defect_success.html')
