498644926d79f05c17a5c410a44da7cb12769a1b
AndyL <u3643279@connect.hku.hk>
2026-03-28 22:47:30 +0800

    Add 'rest_framework' in INSTALLED_APPS for using the Django Rest Framework

--- PJ3297/settings.py
+++ PJ3297/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'BetaTrax.apps.BetatraxConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
