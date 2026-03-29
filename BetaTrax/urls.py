209ea5a5b89e54f9a68109713915784bd0b33029
ouo <laicy0q0@connect.hku.hk>
2026-03-29 17:55:30 +0800

    Update tester dashboard and defect report routes

--- BetaTrax/urls.py
+++ BetaTrax/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 #login as tester
 urlpatterns = [
-    path('', views.dashboard_view, name="dashboard"),
-    path('defectform/', views.defect_view, name="defectform"),
-    path('defectform/success/', views.defect_success_view, name="defect-success"),
+    #login as tester
+    #PBI-01 submite defect report
+    path('tester/', views.dashboard_view, name="tester_dashboard"),
+    path('tester/defectform/', DefectReportCreateView.as_view(),name='defect_form'),
+    path('defectform/success/', views.defect_success_view, name="defect_success"),
     # PBI 4 developer dashboard and fix defect
     path('developer/dashboard/', views.developer_dashboard, name='developer_dashboard'),
     path('developer/fix/<int:defect_id>/', views.fix_defect, name='fix_defect'),
