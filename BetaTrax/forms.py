fe4a496b924dfc911bc1c2ecfcbe0fc9a7c1c3c0
ouo <laicy0q0@connect.hku.hk>
2026-03-29 19:11:03 +0800

    Delete BetaTrax/forms.py

--- BetaTrax/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import DefectReport
-
-class DefectForm(forms.ModelForm):
-    class Meta:
-        model = DefectReport
-        fields = '__all__'
-        exclude = ['status','severity','priority','productowner','developer']
