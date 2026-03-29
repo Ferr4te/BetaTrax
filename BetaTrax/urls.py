d52d69db308d52b2376c08816d13d5bc11a308d6
AndyL <u3643279@connect.hku.hk>
2026-03-29 11:41:55 +0800

    Add 2 paths in urls.py for PBI-04

--- BetaTrax/urls.py
+++ BetaTrax/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.dashboard_view, name="dashboard"),
     path('defectform/', views.defect_view, name="defectform"),
     path('defectform/success/', views.defect_success_view, name="defect-success"),
+    # PBI 4 developer dashboard and fix defect
+    path('developer/dashboard/', views.developer_dashboard, name='developer_dashboard'),
+    path('developer/fix/<int:defect_id>/', views.fix_defect, name='fix_defect'),
 ]
