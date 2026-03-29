d000b19687926fed37cbb685274540baf1856bbf
ouo <laicy0q0@connect.hku.hk>
2026-03-29 19:11:57 +0800

    Add PBI-01,02

--- BetaTrax/serializers.py
+++ BetaTrax/serializers.py
@@ -1,6 +1,18 @@
 from rest_framework import serializers
 from .models import DefectReport
 
+#PBI-01
+class DefectReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DefectReport
+        exclude = ['status', 'severity', 'priority', 'productowner', 'developer']
+
+#PBI-02
+class EvaluateDefectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DefectReport
+        fields = ['status', 'severity', 'priority']
+
 class DefectReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = DefectReport
