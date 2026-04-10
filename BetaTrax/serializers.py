3371573b21e909c2fb4ea503b13260636ac087ba
ouo <laicy0q0@connect.hku.hk>
2026-04-11 04:19:03 +0800

    link duplicate
    Add duplicate_of field to EvaluateDefectSerializer and implement validation for it.

--- BetaTrax/serializers.py
+++ BetaTrax/serializers.py
@@ -7,14 +7,41 @@ class DefectReportSerializer(serializers.ModelSerializer):
         model = DefectReport
         exclude = ['status', 'severity', 'priority', 'productowner', 'developer']
 
-#PBI-02
+
+#PBI-02 + PBI-08
 class EvaluateDefectSerializer(serializers.ModelSerializer):
+    # allow sending the id of the original defect
+    duplicate_of = serializers.PrimaryKeyRelatedField(
+        queryset=DefectReport.objects.all(),
+        required=False,
+        allow_null=True,
+    )
     class Meta:
         model = DefectReport
         fields = ['id', 'title', 'description', 'reproduce_step', 'version', 
-                  'tester_email', 'status', 'severity', 'priority']
+                  'tester_email', 'status', 'severity', 'priority','duplicate_of']
         read_only_fields = ['id', 'title', 'description', 'reproduce_step', 
                            'version', 'tester_email']
+
+    def validate(self, attrs):
+        defect = self.instance  
+        duplicate_of = attrs.get('duplicate_of')
+        # If marking as duplicated, duplicate_of must be provided
+        if attrs.get('status') == DefectReport.CurrentStatus.DUPLICATED:
+            if not duplicate_of:
+                raise serializers.ValidationError(
+                    {'duplicate_of': 'You must specify the original defect this duplicates.'}
+                )
+            if duplicate_of.id == defect.id:
+                raise serializers.ValidationError(
+                    {'duplicate_of': 'A defect cannot be a duplicate of itself.'}
+                )
+            #  Enforce same product
+            if duplicate_of.product_id != defect.product_id:
+                raise serializers.ValidationError(
+                    {'duplicate_of': 'Duplicate must refer to a defect from the same product.'}
+                )
+        return attrs
 
 #PBI-04 and PBI-05
 class DefectReportReadOnlySerializer(serializers.ModelSerializer):
