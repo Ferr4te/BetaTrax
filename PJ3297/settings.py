20d7c75ba75ac6f4b8e19d709e3e08a7b1f877b2
ouo <jenjen9988@gmail.com>
2026-04-25 05:36:48 +0800

    Switch from SQLite to PostgreSQL

--- PJ3297/settings.py
+++ PJ3297/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'PJ3297.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'betatrax',         
+        'USER': 'comp3297',        
+        'PASSWORD': '123',     
+        'HOST': 'localhost',
+        'PORT': '',                 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
