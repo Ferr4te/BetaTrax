ad63f92d779312c4cb99329d3348a2b5006d55ac
LPHTimothy <phliu@connect.hku.hk>
2026-04-25 01:57:30 +0800

    Update testRequestFactoryTest.py basically need change

--- BetaTrax/testRequestFactoryTest.py
+++ BetaTrax/testRequestFactoryTest.py
@@ -27,9 +27,45 @@ class DefectReportViewSetTests(APITestCase):
 		)
 
 	def test_pbi_06_list_defectreport(self):
-		request = self.factory.get(reverse('defect-list'))
+		request = self.factory.get('/api/defects/') #(reverse('defect-list'))??
 		view = DefectReportViewSet.as_view({'get': 'list'})
 		response = view(request)
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
 		self.assertEqual(len(response.data), 1)
 		self.assertIn('Crash on launch', str(response.data))
+
+	def test_pbi_06_retrieve_defectreportdetail(self):
+		request = self.factory.get(f'/api/defects/{self.defect.id}/')
+		view = DefectReportViewSet.as_view({'get': 'retrieve'})
+		response = view(request, pk=self.defect.id)
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		# need add
+		
+	def test_pbi_01_create_defectreport(self):
+		data = {
+			'title':'Login issue',
+			'description':'Cannot login with valid password',
+			'reproduce_step':'Open app; login with valid account',
+			'version':'1.0.0',
+			'product':self.product,
+			'betatester':self.tester,
+			'productowner':self.owner
+		}
+		request = self.factory.post('/api/defects/', data, format='json')
+		view = DefectReportViewSet.as_view({'post': 'create'})
+		response = view(request)
+		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+		self.assertIn('Login issue', str(response.data))
+
+	def test_pbi_02_update_defectreport(self):
+		data = {
+			'status': DefectReport.CurrentStatus.OPEN,
+			'severity': DefectReport.Severity.MAJOR,
+			'priority': DefectReport.Priority.HIGH,
+		}
+		request = self.factory.put(f'/api/defects/{self.defect.id}/', data, format='json')
+		view = DefectReportViewSet.as_view({'put': 'update'})
+		response = view(request, pk=self.product.id)
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		# need add
+		
