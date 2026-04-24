33fc963aa770ffd937e1c489f4f6fe5b10c9bc45
LPHTimothy <phliu@connect.hku.hk>
2026-04-25 01:17:43 +0800

    Update testRequestFactoryTest.py

--- BetaTrax/testRequestFactoryTest.py
+++ BetaTrax/testRequestFactoryTest.py
@@ -14,17 +14,17 @@ class DefectReportViewSetTests(APITestCase):
 		self.dev_user = User.objects.create_user(username='dev1', password='pass123')
 		
 		self.product = Product.objects.create()
+		self.tester = BetaTester.objects.create(user=self.tester_user, email='tester@example.com')
+		self.owner = ProductOwner.objects.create(user=self.po_user, product=self.product)
+		self.developer = Developer.objects.create(user=self.dev_user, product=self.product)
 		self.defectreport = DefectReport.objects.create(
 			title='Crash on launch', 
 			description='App crashes on startup',
 			reproduce_step='Install app; open app',
 			version='1.2.3',
-			product=self.product.id,
-			betatester=self.tester.id
+			product=self.product,
+			betatester=self.tester
 		)
-		self.tester = BetaTester.objects.create(user=self.tester_user, email='tester@example.com')
-		self.owner = ProductOwner.objects.create(user=self.po_user, product=self.product)
-		self.developer = Developer.objects.create(user=self.dev_user, product=self.product)
 
 	def test_pbi_06_list_defectreport(self):
 		request = self.factory.get(reverse('defect-list'))
@@ -32,4 +32,4 @@ class DefectReportViewSetTests(APITestCase):
 		response = view(request)
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
 		self.assertEqual(len(response.data), 1)
-		self.assertContains(response, 'Crash on launch')
+		self.assertIn('Crash on launch', str(response.data))
