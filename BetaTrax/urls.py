0e1a094671be4e17d490e222d82de060b9f870b8
ouo <jenjen9988@gmail.com>
2026-04-25 18:41:32 +0800

    Remove unnecessary urls and htmls

--- BetaTrax/urls.py
+++ BetaTrax/urls.py
@@ -25,23 +25,6 @@ urlpatterns = [
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/',SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
-
-
-    #login as tester
-    #PBI-01 submite defect report
-    path('tester/', views.dashboard_view, name="tester_dashboard"),
-    path('tester/defectform/', DefectReportCreateView.as_view(),name='defect_form'),
-    path('defectform/success/', views.defect_success_view, name="defect_success"),
-    
-    #PBI-02 Evaluate and accept defect for Product owner =================================
-    path('owner/', views.owner_dashboard_view, name="owner_dashboard"),
-    path('owner/evaluate/', views.evaluate_defect_page, name="owner_defect"),
-    path('owner/evaluate/<int:pk>/', evaluate_defect_update_view.as_view(), name="owner_defect_evaluate"),
-
-    #PBI-03 Select defect to work on
-    path('developer/', views.developer_dashboard_view, name='developer_dashboard'),
-    path('developer/defects/<int:pk>/assign/', views.assign_defect_view, name='assign_defect'),
-
     # Delete url for PBI-04 and PBI-05 since fix and resolve inside
     # DefectReportViewSet already can access to PBI-04 and PBI-05 pages
 
