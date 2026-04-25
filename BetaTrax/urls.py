22c3f007748d186bdd763953a20b26dbdd11855e
LPHTimothy <phliu@connect.hku.hk>
2026-04-25 18:38:52 +0800

    Update urls.py add pbi-18

--- BetaTrax/urls.py
+++ BetaTrax/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from . import views
-from .views import DefectReportCreateView, DefectReportViewSet, ProductViewSet, CommentViewSet
+from .views import DefectReportCreateView, DefectReportViewSet, ProductViewSet, CommentViewSet, DeveloperEffectivenessViewSet, DeveloperViewSet
 from .views import evaluate_defect_update_view
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested import routers
