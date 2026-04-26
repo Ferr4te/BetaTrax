1e4790aeeee8563d21c3d733474ed44dd6169b62
AndyL <u3643279@connect.hku.hk>
2026-04-26 14:20:29 +0800

    Change the path under "tests" file and all 4 tests pass

--- BetaTrax/tests/testRequestFactoryTest.py
+++ BetaTrax/tests/testRequestFactoryTest.py
@@ -3,8 +3,8 @@ from rest_framework import status
 from django.urls import reverse
 from django.contrib.auth.models import User
 import json
-from .views import DefectReportViewSet, ProductViewSet
-from .models import BetaTester, DefectReport, Developer, Product, ProductOwner, Comment
+from BetaTrax.views import DefectReportViewSet, ProductViewSet
+from BetaTrax.models import BetaTester, DefectReport, Developer, Product, ProductOwner, Comment
 from django_tenants.test.cases import TenantTestCase
 from django_tenants.utils import tenant_context
 from django.db import connection
@@ -15,12 +15,11 @@ from rest_framework.test import force_authenticate
 class DefectReportViewSetTests(TenantTestCase):
 	def setUp(self):
 		super().setUp()
-		connection.set_tenant(self.tenant)
 		with tenant_context(self.tenant):
 			self.tester_user = User.objects.create_user(username='tester1', password='pass123')
 			self.po_user = User.objects.create_user(username='po1', password='pass123')
 			self.dev_user = User.objects.create_user(username='dev1', password='pass123')
-			
+
 			self.product = Product.objects.create(name='Test Product')
 			self.tester = BetaTester.objects.create(user=self.tester_user, email='tester@example.com')
 			self.owner = ProductOwner.objects.create(user=self.po_user, product=self.product)
@@ -33,13 +32,16 @@ class DefectReportViewSetTests(TenantTestCase):
 				product=self.product,
 				betatester=self.tester
 			)
+		connection.set_tenant(self.tenant)
+		self.factory = APIRequestFactory()
 
 	def test_pbi_06_list_defectreport(self):
 		request = self.factory.get('/api/defects/') #(reverse('defect-list'))??
 		view = DefectReportViewSet.as_view({'get': 'list'})
 		response = view(request)
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
-		self.assertEqual(len(response.data), 1)
+		self.assertEqual(response.data['count'], 1)
+		self.assertEqual(len(response.data['results']), 1)
 		self.assertIn('Crash on launch', str(response.data))
 
 	def test_pbi_06_retrieve_defectreportdetail(self):
